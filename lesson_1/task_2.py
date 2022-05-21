e13a166179192d96ca292f2af45ec447169085dc
VidRnd <yavidrnd@yandex.ru>
2022-05-21 16:07:07 +0300

    add

--- lesson_1/task_2.py
+++ lesson_1/task_2.py
@@ -1,6 +1,5 @@
 sum_all = 0
 sum_plus = 0
-sum = []
 for i in range(1,1001):
     num_for_sum = (i**3)
     num = num_for_sum
@@ -10,7 +9,6 @@ for i in range(1,1001):
         suma_num = suma_num + digit
         num = num // 10
     if suma_num%7 == 0 :
-        sum.append(num_for_sum)
         sum_all = sum_all + num_for_sum
     num = int(str(i**3)+"17")
     suma_num = 0
@@ -22,7 +20,7 @@ for i in range(1,1001):
         sum_plus = sum_plus + num_for_sum
 print("Сумма -", sum_all)
 print("Сумма добавляем '17' к каждому числу -", sum_plus)
-print(sum)
+
 
 
 
