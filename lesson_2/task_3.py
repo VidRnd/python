b0c9cbd0f4b96d5354fddc420116557b96a06293
VidRnd <97319349+VidRnd@users.noreply.github.com>
2022-05-25 00:44:35 +0300

    Delete task_3.py

--- lesson_2/task_3.py
+++
@@ -1,5 +0,0 @@
-result_lst = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
-for i in result_lst:
-    element_one_lst = list(i.split(' '))
-    print("Привет,"+element_one_lst[-1].capitalize()+"!")
-
