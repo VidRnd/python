ff3fc80c3fde0f93de6faed8f82e87fcdc5ddeb5
VidRnd <97319349+VidRnd@users.noreply.github.com>
2022-05-25 02:12:14 +0300

    add

--- lesson_2/task_2.py
+++ lesson_2/task_2.py
@@ -1,6 +1,4 @@
 rezult_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-answer_one = ['в', '"', '05', '"', 'часов', '"', '17', '"', 'минут', 'температура', 'воздуха', 'была', '"', '+05', '"', 'градусов']
-answer_two = 'в "05" часов "17" минут температура воздуха была "+05" градусов'
 for i in rezult_list[:]:
     index_rez_lst = rezult_list.index ( f"{i}" )
     if i[0] == '+':
@@ -17,11 +15,8 @@ for i in rezult_list[:]:
             rezult_list.insert ( index_rez_lst + 2 , '"' )
 
         else:
-            print(index_rez_lst,i)
             rezult_list.insert ( index_rez_lst , '"' )
             rezult_list.insert ( index_rez_lst + 2 , '"' )
 
-print("Должно получится\n",answer_one)
 print(rezult_list)
-print("Должно получится\n",answer_two)
 print(" ".join(rezult_list))
