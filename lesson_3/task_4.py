3abaccb02fca439cdb496c45f08c6b2c54240d9f
VidRnd <97319349+VidRnd@users.noreply.github.com>
2022-05-28 03:28:31 +0300

    add

--- lesson_3/task_4.py
+++ lesson_3/task_4.py
@@ -5,16 +5,16 @@ def thesaurus_adv(*args):
     for i in args:
         user_lst.append( i.split(' '))
     for surname in user_lst :
-        key = surname[1][0]
-        if key not in rez_sorting :
-            rez_sorting[ key ] = [ ]
-        rez_sorting[ key ].append ( surname )
+        key_surname = surname[1][0]
+        if key_surname not in rez_sorting :
+            rez_sorting[key_surname] = []
+        rez_sorting[key_surname].append(surname)
     for i in rez_sorting.keys():
         res_sort_name = {}
         for g in rez_sorting[i]:
             sort_name = g[ 0 ][ 0 ]
             if sort_name not in res_sort_name :
-                res_sort_name[ sort_name ] = [ ]
+                res_sort_name[ sort_name ] = []
             connect_text= g[0]+", "+g[1]
             res_sort_name[sort_name].append (connect_text )
         rez_sorting[i]=(res_sort_name)
