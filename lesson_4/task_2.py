11cc34ccdf3b5b2b93664f219c0794004f444558
VidRnd <97319349+VidRnd@users.noreply.github.com>
2022-06-01 19:50:25 +0300

    add

--- lesson_4/task_2.py
+++ lesson_4/task_2.py
@@ -1,18 +1,14 @@
 import requests
-import pprint
-import  json
 import datetime as dt
-
 
 def currency_rates(money):
     URL = 'https://www.cbr-xml-daily.ru/daily_json.js'
-
     response = requests.get ( URL )
-    h=response.json()
-    date_time_str = h['Date']
+    answer_json=response.json()
+    date_time_str = answer_json['Date']
     date_time_obj = dt.datetime.strptime(date_time_str, '%Y-%m-%dT%H:%M:%S%z')
-    answer = f"Время: {date_time_obj.strftime('%Y-%m-%d %H:%M:%S')}\nВалюта: {(h['Valute'][f'{money}']['Name'] )}\n" \
-             f"Стоимость: {h['Valute'][f'{money}']['Value']} руб"
+    answer = f"Время: {date_time_obj.strftime('%Y-%m-%d %H:%M:%S')}\nВалюта: {(answer_json['Valute'][f'{money}']['Name'] )}\n" \
+             f"Стоимость: {answer_json['Valute'][f'{money}']['Value']} руб"
     return answer
 
 print(currency_rates("ZAR"))
