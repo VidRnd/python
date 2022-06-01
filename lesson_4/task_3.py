1d164d6699e9beed5f0a504488b46d86a2a05dc9
VidRnd <97319349+VidRnd@users.noreply.github.com>
2022-06-01 20:01:39 +0300

    add

---
+++ lesson_4/task_3.py
@@ -0,0 +1,22 @@
+import requests
+import datetime as dt
+
+
+def currency_rates(money):
+    """Запрашиваем на сайте курс валют"""
+    URL = 'https://www.cbr-xml-daily.ru/daily_json.js'
+    response = requests.get ( URL )
+    answer_json=response.json()
+    if dict( answer_json['Valute'] ).get(f'{money.upper()}',None) is None:
+        return None
+    else:
+        date_time_str = answer_json['Date']
+        date_time_obj = dt.datetime.strptime(date_time_str, '%Y-%m-%dT%H:%M:%S%z')
+        answer = f"Время: {date_time_obj.strftime('%Y-%m-%d %H:%M:%S')}\n" \
+                 f"Валюта: {(answer_json['Valute'][f'{money.upper()}']['Name'] )}\n" \
+                 f"Стоимость: {answer_json['Valute'][f'{money.upper()}']['Value']} руб"
+        return answer
+
+print(currency_rates(input("Введите курс валют в виде ZAR")))
+
+
