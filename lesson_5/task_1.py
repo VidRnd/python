6288698c5091d9a70956fb995dd7ba16c6e26762
VidRnd <97319349+VidRnd@users.noreply.github.com>
2022-06-03 21:58:45 +0300

    add

--- lesson_5/task_1.py
+++ lesson_5/task_1.py
@@ -1,9 +1,10 @@
 def odd_nums(max_num):
-    for num in range ( 1 , max_num ):
+    for num in range ( 0 , max_num+1 ):
         if num % 2 != 0:
             yield num
 
 odd_to_15 = odd_nums(15)
+print(odd_to_15)
 print(next(odd_to_15))
 print(next(odd_to_15))
 print(next(odd_to_15))
@@ -11,6 +12,8 @@ print(next(odd_to_15))
 print(next(odd_to_15))
 print(next(odd_to_15))
 print(next(odd_to_15))
+print(next(odd_to_15))
+
 
 
 
