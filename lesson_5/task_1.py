daa4f00d6b6db1ddf5583574f3aae3e201a918a0
VidRnd <97319349+VidRnd@users.noreply.github.com>
2022-06-04 03:16:19 +0300

    add

--- lesson_5/task_1.py
+++ lesson_5/task_1.py
@@ -1,8 +1,12 @@
 def odd_nums(max_num):
     """Генерируем нечетные числа от 0 до n, с помощью генератора"""
     for num in range ( 0 , max_num+1 ):
-        if num % 2 != 0:
-            yield num
+        try:
+            if num % 2 != 0:
+                yield num
+        except StopIteration:
+            print("dddddd")
+            return
 
 odd_to_15 = odd_nums(15)
 print(odd_to_15)
@@ -15,6 +19,9 @@ print(next(odd_to_15))
 print(next(odd_to_15))
 print(next(odd_to_15))
 
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
 
 
 
